9ba1a34948835e5561961e0d9168f657dc0ece93
atam84 <amine.tamdy@gmail.com>
2019-12-24 15:15:20 -0500

    start working ...

--- client/main.js
+++ client/main.js
@@ -1,23 +1,48 @@
 import 'bootstrap';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict'
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import './main.html';
+import '../lib/routes.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+const _display = new ReactiveVar();
+const _data = new ReactiveDict();
+
+
+Template.List_namespaces.helpers({
+  get_listNameSpaces() {
+    return _data.get('namespaces');
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.mainMenu.events({
+  'click .get-namespaces': (e, instance) => {
+    e.preventDefault();
+    console.log("Ask for namespaces list");
+    Meteor.call('getNameSpaces', (err, res) => {
+      if (err) {
+        console.log("ERROR : " + err);
+      } else {
+        console.dir(res.items);
+        _data.set('namespaces', res.items);
+      }
+    });
   },
+  'click .get-cluster': (e, instance) => {
+    e.preventDefault();
+    console.log("Ask for cluster information");
+    Meteor.call('getNodes', (err, res) => {
+      if (err) {
+        console.log("ERROR : " + err);
+      } else {
+        console.dir(res);
+        _data.set('cluster', res);
+      }
+    });
+  }
 });
+
--- server/main.js
+++ server/main.js
@@ -1,5 +1,27 @@
 import { Meteor } from 'meteor/meteor';
+//import { k8s } from '@kubernetes/client-node';
+
+const k8s = require('@kubernetes/client-node');
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+    'getNameSpaces': () => {
+        console.log('*** Serving k8s namespaces');
+        return k8sApi.listNamespace().then((res) => {
+          return res.body;
+        });
+    },
+    'getNodes': () => {
+        console.log('*** Serving Cluster nodes');
+        return k8sApi.listNode().then((res) => {
+          return res.body;
+        });
+    }
 });
